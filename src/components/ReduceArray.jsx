1fa1fcd9a5c655a7239915b04529299a1713ba35
ale <alexlab84@hotmail.com>
2024-11-19 16:04:31 +0100

    Add changes in component ReduceArray

---
+++ src/components/ReduceArray.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function ReduceArray() {
+    const numbers = [10, 20, 30, 40, 50];
+    const[totalSum, setTotalSum] = useState(null);
+  
+    return (
+    
+
+    <>
+    </>
+  );
+}
+
+export default ReduceArray;
